daf8f6b7aae1bd0c063525cd5d4be49f0b9a6ef6
= <renatomdavid811@hotmail.com>
2024-06-08 18:47:15 -0300

    criando operadores condicionais, blocos de codigo escopo e variaveis

---
+++ operadorCondicional/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Concluir uma atividade de desafio usando expressões boolianas
+ Desafio da lógica de decisão
+Neste desafio, você implementará a lógica de decisão com base em uma série de regras de negócios.
+As regras de negócios especificam o acesso que será concedido aos usuários com base nas respectivas
+permissões baseadas em função e no nível de carreira. Os branches de código exibirão uma mensagem
+diferente para cada usuário, de acordo com as respectivas permissões e o nível.*/
+/*
+Random coin = new Random();
+
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");*/
+
+
+string permission = "Admin|Manager";
+int level = 19;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 60)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
