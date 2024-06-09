bc0859c6d4a0cb7760193005b96fd1b6f316b6d7
= <renatomdavid811@hotmail.com>
2024-06-08 21:46:11 -0300

    criando dados para cadastramento de cães e gatos

---
+++ ContosoPets/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[,] ourAnimals = new string[5, 6]
+        {
+            { "1", "cão", "2", "pequeno, pelagem preta", "brincalhão", "Rex" },
+            { "2", "gato", "1", "médio, pelagem branca", "calmo", "Mimi" },
+            { "3", "cão", "3", "grande, pelagem marrom", "protetor", "Max" },
+            { "4", "gato", "4", "pequeno, pelagem cinza", "curioso", "Luna" },
+            { "5", "cão", "1", "médio, pelagem dourada", "amigável", "Buddy" }
+        };
+
+        while (true)
+        {
+            Console.WriteLine("\nMenu Principal:");
+            Console.WriteLine("1. Listar todas as nossas informações atuais sobre os animais de estimação.");
+            Console.WriteLine("2. Atribuir valores aos campos da matriz ourAnimals.");
+            Console.WriteLine("Digite 'Exit' para sair do programa.");
+
+            string choice = Console.ReadLine();
+
+            if (choice.ToLower() == "exit")
+            {
+                break;
+            }
+
+            switch (choice)
+            {
+                case "1":
+                    ListAllAnimals(ourAnimals);
+                    break;
+                case "2":
+                    AddNewAnimal(ref ourAnimals);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Por favor, tente novamente.");
+                    break;
+            }
+        }
+    }
+
+    static void ListAllAnimals(string[,] animals)
+    {
+        Console.WriteLine("\nLista de todos os animais:");
+        for (int i = 0; i < animals.GetLength(0); i++)
+        {
+            for (int j = 0; j < animals.GetLength(1); j++)
+            {
+                Console.Write($"{animals[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void AddNewAnimal(ref string[,] animals)
+    {
+        int newId = animals.GetLength(0) + 1;
+        string[,] tempArray = new string[newId, 6];
+        for (int i = 0; i < animals.GetLength(0); i++)
+        {
+            for (int j = 0; j < animals.GetLength(1); j++)
+            {
+                tempArray[i, j] = animals[i, j];
+            }
+        }
+
+        Console.Write("Espécie do animal (cão ou gato): ");
+        string species = Console.ReadLine();
+        Console.Write("Idade do animal (em anos): ");
+        string age = Console.ReadLine();
+        Console.Write("Descrição das condições/características físicas do animal: ");
+        string physicalDesc = Console.ReadLine();
+        Console.Write("Descrição da personalidade do animal: ");
+        string personalityDesc = Console.ReadLine();
+        Console.Write("Apelido do animal: ");
+        string nickname = Console.ReadLine();
+
+        tempArray[newId - 1, 0] = newId.ToString();
+        tempArray[newId - 1, 1] = species;
+        tempArray[newId - 1, 2] = age;
+        tempArray[newId - 1, 3] = physicalDesc;
+        tempArray[newId - 1, 4] = personalityDesc;
+        tempArray[newId - 1, 5] = nickname;
+
+        animals = tempArray;
+
+        Console.WriteLine("Novo animal adicionado com sucesso!");
+    }
+}
---
+++ diferencaDoWhile/Program.cs
@@ -0,0 +1,65 @@
+﻿// utilizando o DO 
+string? readResult;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        valueEntered = readResult;
+    }
+
+    validNumber = int.TryParse(valueEntered, out numValue);
+
+    if (validNumber == true)
+    {
+        if (numValue <= 5 || numValue >= 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+readResult = Console.ReadLine();
+
+/*utilizando o WHILE*/
+string? Result;
+string roleName = "";
+bool validEntry = false;
+
+do
+{
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    Result = Console.ReadLine();
+    if (Result != null)
+    {
+        roleName = Result.Trim();
+    }
+
+    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user")
+    {
+        validEntry = true;
+    }
+    else
+    {
+        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+    }
+
+} while (validEntry == false);
+
+Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+Result = Console.ReadLine();
+/*Uitlizando o FOR*/
+
