57f8de515371c74d81234c9cfb386aa0aef92c10
= <renatomdavid811@hotmail.com>
2024-06-13 20:55:36 -0300

    converter tipos de dados usando tecnicas de conversão em C#

---
+++ conversaoDeTipoDeDados/Program.cs
@@ -0,0 +1,103 @@
+﻿/*decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
+
+Use ToString() para converter um número em um string
+
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+
+Converter um string em um int usando o método auxiliar Parse()
+
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum);
+
+Converter um string em um int usando a classe Convert
+
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+
+
+int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value2);
+
+Exercício – Examinar o método TryParse()
+
+string name = "Bob";
+Console.WriteLine(int.Parse(name));
+
+ Utilizar o método TryParse() para analisar uma cadeia de caracteres como um int
+
+
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+
+if (result > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
+Exercício – Concluir um desafio em que é preciso combinar valores de matriz de cadeia de caracteres como cadeias de caracteres e inteiros
+ 
+ 
+
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
+
+*/
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = Convert.ToInt32(value1/ value2);
+Console.WriteLine($"Divide value1 by value2, display result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the resultado as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float:{result3}");
