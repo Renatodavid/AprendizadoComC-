a2cbe16463234094c71983d7d05aa4a5f145f8da
= <renatomdavid811@hotmail.com>
2024-06-18 19:24:32 -0300

    finalizando exibindo endereço de email

---
+++ exibirEnderecosDeEmail/Program.cs
@@ -0,0 +1,24 @@
+﻿string[,] corporate =
+{
+     {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for  (int i = 0; i < corporate.GetLength(0); i++)
+{
+   Console.WriteLine($"{corporate[i, 0]} {corporate[i, 1]}@{externalDomain}");
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+   Console.WriteLine($"{external[i, 0]} {external[i, 1]}@{externalDomain}");
+}
