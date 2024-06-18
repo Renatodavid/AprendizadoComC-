37db0d2883a80b58179079fe67748acbc539fa15
= <renatomdavid811@hotmail.com>
2024-06-18 18:37:29 -0300

    finalizando metodo parametros opcionais

--- usarParametrosEmMetodos/Program.cs
+++ usarParametrosEmMetodos/Program.cs
@@ -149,29 +149,25 @@ void SetHealth(bool isHealthy)
     Console.WriteLine($"Middle: {status}");
 }
 
-/*Métodos com parâmetros opcionais  Criar um aplicativo RSVP*/
+/*Métodos com parâmetros opcionais  Criar um aplicativo RSVP
 
-RSVP("Rebecca", 1, "none", true);
-RSVP("Nadia", 2, "Nuts", true);
-RSVP("Linh", 2, "none", false);
-RSVP("Tony", 1, "Jackfruit", true);
-RSVP("Noor", 4, "none", false);
-RSVP("Jonte", 2, "Stone fruit", false);
-ShowRSVPs();
 
 string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
 string[] rsvps = new string[10];
 int count = 0;
 
-void RSVP(string name, int partySize, string allergies, bool inviteOnly)
-{
-    if (inviteOnly)
-    {
-        // search guestList before adding rsvp
-    }
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
 
-    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-    count++;
+
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+{
+   
     if (inviteOnly)
     {
         bool found = false;
@@ -189,6 +185,9 @@ void RSVP(string name, int partySize, string allergies, bool inviteOnly)
             return;
         }
     }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
 }
 
 void ShowRSVPs()
@@ -198,4 +197,6 @@ void ShowRSVPs()
     {
         Console.WriteLine(rsvps[i]);
     }
-}
+}*/
+
+
